c6f0c96e731f81b5e687630b671127ce6ad5d2ad
Neppoli <tneppoli@gmail.com>
2022-04-05 09:45:46 +0530

    new

---
+++ src/Data.java
@@ -0,0 +1,18 @@
+
+public class Data {
+	public static void main(String[] args) {
+byte age=26;
+short mark=5400;
+int accNo=987654321;
+long phoneNo=8883846512l;
+float percentage=13.45636f;
+double decimal=99.9999999999999;
+char gender='F';
+String name="Neppoli";
+boolean b=true;
+boolean c=false;
+System.out.println("Age:"+age+"\n"+"Mark:"+mark+"\n"+"AccNo:"+accNo+"\n"+"Phone:"+phoneNo+"\n"+"\n"+"percentage:"+percentage+"\n"+"Decimal:"+decimal+"\n"+"\n"+"gender:"+gender+"\n"+"Name:"+name+"\n"+"Boolean:"+b+"\n"+"Boolean:"+c);
+}
+
+}
+
---
+++ src/Sample.java
@@ -0,0 +1,16 @@
+
+public class Sample {
+public static void main(String[] args) {
+	byte seriolNo=3;
+	short total=1156;
+	int idNo=2356789;
+	long accNo=5416789863421l;
+	float passRate=97.3f;
+	double failPercent=2.777777777777;
+	char gender='f';
+	String name="Rocky";
+	boolean pass=true;
+	boolean fail=false;
+	System.out.println("Seriol No:"+seriolNo+"\n"+"IDno:"+idNo+"\n"+"Total:"+total+"\n"+"Long:"+accNo+"\n"+"PassRate:"+passRate+"\n"+"Failpercentage:"+failPercent+"\n"+"gender:"+gender+"\n"+"name:"+name+"\n"+"Pass:"+pass+"Fail:"+fail);
+}
+}
